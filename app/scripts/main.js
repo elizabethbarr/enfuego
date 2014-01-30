13428a6ec067895d0f89057a4a193ef63cbdc959
Matt Bertino <mattbertino@gmail.com>
2014-01-30 18:07:31 -0500

    got the first userdata js function working. need to work on the input new functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,39 @@
-console.log('\'Allo \'Allo!');
+userData = [
+  {
+    avatarusername: 'Bertman45',
+    avataremail: 'bertman45@gmail.com',
+    avatarlocation: 'Greenville',
+    image: 'https://lh6.googleusercontent.com/-jaKUk5hHDfo/AAAAAAAAAAI/AAAAAAAAAAA/Ku-ZY_hmtwE/s48-c-k-no/photo.jpg'
+  },
+  {
+    avatarusername: 'Jeremyson',
+    avataremail: 'jcrawford.design@gmail.com',
+    avatarlocation: 'Greenville',
+    image: 'https://lh6.googleusercontent.com/-jaKUk5hHDfo/AAAAAAAAAAI/AAAAAAAAAAA/Ku-ZY_hmtwE/s48-c-k-no/photo.jpg'
+  },
+  {
+    avatarusername: 'Elizbarr',
+    avataremail: 'elizbarr@gmail.com',
+    avatarlocation: 'Greenville',
+    image: 'https://lh6.googleusercontent.com/-jaKUk5hHDfo/AAAAAAAAAAI/AAAAAAAAAAA/Ku-ZY_hmtwE/s48-c-k-no/photo.jpg'
+    // images: [
+    //   {
+    //     url:''
+    //   },
+    //   {
+    //     url:''
+    //   },
+    // ]    
+  }
+]
+ 
+var useravatartemplate = _.template($('.avatar-template').text());
+
+ _.each(userData, function(user,index){
+  $('.ceneteravatarcontainer').append(useravatartemplate(user));
+ });
+
+
+$('.avatar').hover(function() {
+  $(this).children('.caption').slideDown('slow')
+});
